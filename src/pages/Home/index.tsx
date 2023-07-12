a6182ad6225561f01426eef084d982cdaa3d6144
Maximilian Harrisson <maximilianharrisson@gmail.com>
2023-07-12 20:44:58 -0300

    Add presentation card for small screens

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,17 @@
+import "./styles.css"
+
 const Home = () => {
   return (
     <>
-      <div>
-        Início
+      <div className="p-5 bg-secondary-subtle rounded-3 d-block d-md-none text-center mt-4 mb-4">
+        <div className="card bg-dark text-white">
+          <img src="assets/img/background/pc-3.png" className="w-100" alt="Desenho de um Computador" />
+          <div className="card-img-overlay">
+            <h5 className="card-text">
+              Site desenvolvido com a proposta de ajudar a montar o computador perfeito para você.
+            </h5>
+          </div>
+        </div>
       </div>
     </>
   );
