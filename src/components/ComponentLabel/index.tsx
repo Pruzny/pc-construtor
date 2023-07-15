97ad559c41a145d92f2016e1309bbf48a1daba09
Maximilian Harrisson <maximilianharrisson@gmail.com>
2023-07-15 14:05:14 -0300

    Remove underline from link

--- src/components/ComponentLabel/index.tsx
+++ src/components/ComponentLabel/index.tsx
@@ -11,11 +11,11 @@ const ComponentLabel = ({ name, raw, isActive }: Props) => {
   return (
     <>
       <li className="nav-item d-flex" role="presentation">
-        <NavLink to={`/montagem/${raw}`} className="flex-fill">
+        <Link to={`/montagem/${raw}`} className="flex-fill">
           <button className={`nav-link btn btn-light flex-fill w-100 ${isActive ? "active" : ""}`} id={`${raw}-tab`} data-bs-toggle="tab"
             data-bs-target={`#${raw}Options`} type="button" role="tab" aria-controls={`${raw}Options`}
             aria-selected="true">{name}</button>
-        </NavLink>
+        </Link>
       </li>
     </>
   );
